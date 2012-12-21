e910e12ae42126a12c456f16ffb52f1a898985d2
Anthony O'Hare <ohare.anthony@gmail.com>
2012-12-21 21:48:12 +0000

    Added tests for statistics distributions.

---
+++ src/test/java/broadwick/statistics/distributions/BroadwickStatisticsDistributionsTestSuite.java
@@ -0,0 +1,33 @@
+package broadwick.statistics.distributions;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({broadwick.statistics.distributions.IntegerDistributionTest.class})
+public class BroadwickStatisticsDistributionsTestSuite {
+    
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+}
