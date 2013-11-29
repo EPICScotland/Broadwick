a3b6242eb3cce75708cda2551c0bb47e79187d8d
Anthony O'Hare <ohare.anthony@gmail.com>
2013-11-29 12:25:34 +0000

    Created archetype for creating new project built on the Broadwick framework. Under the archetype directory simply ‘mvm install’ the project and then a new project can be created using ‘mvn3 archetype:generate -DarchetypeCatalog=local’. Alternatively, the archetype can be installed from the jar file as ‘mvn install:install-file -DgroupId=Broadwick -DartifactId=Broadwick-archetype -Dversion=1.0 -Dpackaging=jar -Dfile=/path/to/file’

---
+++ archetype/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,28 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+
+import broadwick.model.Model;
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+public class App extends Model {
+
+    @Override
+    public final void init() {
+	log.info("Initialising project");
+    }
+
+    @Override
+    public final void run() {
+	log.info("Running project");
+    }
+
+    @Override
+    public final void finalise() {
+	log.info("Closing project");
+    }
+
+}
+
