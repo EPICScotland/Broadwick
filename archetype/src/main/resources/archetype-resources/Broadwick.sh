e5c93470e6b7ab7ba3f54bc759eef8730657971d
Anthony O'Hare <anthony.ohare@stir.ac.uk>
2013-12-11 14:06:18 +0000

    added package-info.java to the archetype.

--- archetype/src/main/resources/archetype-resources/Broadwick.sh
+++ archetype/src/main/resources/archetype-resources/Broadwick.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-java -server -Xmx4096m -XX:-UseGCOverheadLimit -XX:MaxPermSize=2048m -XX:+UseParallelGC -XX:+UnlockCommercialFeatures -XX:+FlightRecorder -Done-jar.silent=true -jar target/__artifactId__-__version__.one-jar.jar $*
+java -server -Xmx4096m -XX:-UseGCOverheadLimit -XX:MaxPermSize=2048m -XX:+UseParallelGC -XX:+UnlockCommercialFeatures -XX:+FlightRecorder -Done-jar.silent=true -jar target/${artifactId}-${version}.one-jar.jar $*
 
