f43cfbf6ea45ecc79c188150f3a658c835691e93
Anthony O'Hare <ohare.anthony@gmail.com>
2014-05-27 15:37:09 +0100

    Replaced CloneUtils class with library that does not required cloned objects to implement synchronised.

--- src/main/java/broadwick/utils/CloneUtils.java
+++ src/main/java/broadwick/utils/CloneUtils.java
@@ -15,12 +15,7 @@
  */
 package broadwick.utils;
 
-import com.google.common.base.Throwables;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import com.rits.cloning.Cloner;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -39,31 +34,15 @@ public final class CloneUtils {
     }
 
     /**
-     * Provides a deep clone serializing/de-serializing
-     * <code>objToClone</code>.
-     * @param objToClone The object to be cloned.
+     * Deep clones an object. The object to be clone does not need to implement Cloneable or be serializable
+     * for this method to work.
+     * @param <T> the type of "o"
+     * @param o The object to be cloned.
      * @return The cloned object.
      */
     @SuppressWarnings("FinalStaticMethod")
-    public static Object deepClone(final Object objToClone) {
-        Object obj = null;
-        try {
-            // Write the object out to a byte array
-            final ByteArrayOutputStream bos = new ByteArrayOutputStream();
-            try (ObjectOutputStream out = new ObjectOutputStream(bos)) {
-                out.writeObject(objToClone);
-                out.flush();
-            }
-
-            // Make an input stream from the byte array and read
-            // a copy of the object back in.
-            final ObjectInputStream in = new ObjectInputStream(
-                    new ByteArrayInputStream(bos.toByteArray()));
-            obj = in.readObject();
-        } catch (IOException | ClassNotFoundException e) {
-            log.error(Throwables.getStackTraceAsString(e));
-        }
-        return obj;
+    public static <T extends Object> T deepClone(final T o) {
+        return (new Cloner()).deepClone(o);
     }
 
 }
