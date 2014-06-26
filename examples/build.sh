0e1607c87fcc77b838708446fadeeb28c562445a
Anthony O'Hare <ohare.anthony@gmail.com>
2014-06-26 15:38:14 +0100

    Updated the examples build script. It will now compile, analyse and run each example.

--- examples/build.sh
+++ examples/build.sh
@@ -11,6 +11,8 @@ do
 	mvn3 clean package | tee -a build.log
 	echo "Analysing $f" | tee -a build.log
 	mvn3 sonar:sonar | tee -a build.log
+	echo "Running $f" | tee -a build.log
+	./Broadwick.sh -c Broadwick.xml | tee -a build.log
 	cd -;
 done
  
