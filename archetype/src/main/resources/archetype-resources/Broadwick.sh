210c5bd6c0fac9e1851a5e42d9311fe347ec189c
Anthony O'Hare <ohare.anthony@gmail.com>
2015-06-10 08:25:29 +0100

    Added comment to archetype

--- archetype/src/main/resources/archetype-resources/Broadwick.sh
+++ archetype/src/main/resources/archetype-resources/Broadwick.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
+
+# TODO - replace the ${artifactId} and ${version} with your projects artifactId and version.
+
 java -server -Xmx4096m -XX:-UseGCOverheadLimit -XX:MaxPermSize=2048m -XX:+UseParallelGC -XX:+UnlockCommercialFeatures -XX:+FlightRecorder -Done-jar.silent=true -jar target/${artifactId}-${version}.one-jar.jar $*
 
