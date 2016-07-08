e3e5a3a45903e42fd485ed59f992e038fd23fdf8
Anthony O'Hare <ohare.anthony@gmail.com>
2016-07-08 13:20:50 +0100

    Removed unused parameter in command line call to Java

--- archetype/src/main/resources/archetype-resources/Broadwick.sh
+++ archetype/src/main/resources/archetype-resources/Broadwick.sh
@@ -2,5 +2,5 @@
 
 # TODO - replace the ${artifactId} and ${version} with your projects artifactId and version.
 
-java -server -Xmx4096m -XX:-UseGCOverheadLimit -XX:MaxPermSize=2048m -XX:+UseParallelGC -XX:+UnlockCommercialFeatures -XX:+FlightRecorder -Done-jar.silent=true -jar target/${artifactId}-${version}.one-jar.jar $*
+java -server -Xmx4096m -XX:-UseGCOverheadLimit -XX:+UseParallelGC -XX:+UnlockCommercialFeatures -XX:+FlightRecorder -Done-jar.silent=true -jar target/${artifactId}-${version}.one-jar.jar $*
 
